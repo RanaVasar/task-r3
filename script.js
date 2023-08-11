6db7d1579dae3bb86a5777549b63d1fad9ceca18
RanaVasar <rdvasar@gmail.com>
2023-08-11 01:55:11 -0700

    third commit

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function classToggle() {
   
   document.querySelector('.btn-logo')
     .addEventListener('click', classToggle);
+
+   
+
+   var slide = document.getElementById("slider")
+   var btn1 = document.getElementById("dot1")
+   var btn2 = document.getElementById("dot2")
+   var btn3 = document.getElementById("dot3")
+
+   btn1.onclick = function(){
+    slide.style.transform = "translateX(50px)";
+   }
+   btn2.onclick = function(){
+    slide.style.transform = "translateX(-100px)";
+   }
+   btn3.onclick = function(){
+    slide.style.transform = "translateX(-300px)";
+   }
